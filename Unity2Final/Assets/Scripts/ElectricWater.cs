2d3e7b63eb9a29bb68b1fe3f54285132471cabdd
Victor Lopez <115019151+ScuffedUnknown@users.noreply.github.com>
2023-12-11 13:26:51 -0500

    Need Electricity and Adversary

--- Unity2Final/Assets/Scripts/ElectricWater.cs
+++ Unity2Final/Assets/Scripts/ElectricWater.cs
@@ -24,7 +24,7 @@ public class ElectricWater : MonoBehaviour
 
     IEnumerator WaterDamage()
     {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(1.5f);
         player.GetComponent<PlayerHealth>().TakeDamage(20);
         StartCoroutine(WaterDamage());
     }
